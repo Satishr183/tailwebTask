61f2b51cb325063f3b179f01f2028336f864ac60
Satish <satish_95raj@outlook.com>
2022-12-10 23:04:47 +0530

    Task almost done

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+const route = require('./routes/route.js');
+
+
+const app = express()
+
+app.use(express.json())
+
+
+mongoose.connect("mongodb+srv://Satishr183:6SdvyY7phCfH5VsV@cluster0.ewmx8.mongodb.net/Tailwebs_DB", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+
+
+app.use('/', route);
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
